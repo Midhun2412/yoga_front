9111f3553652840fd13ef4674dddf144aea2e3bd
online <mohanmidhun084@gmail.com>
2023-12-20 11:10:07 +0530

    connected login to backend

--- yoga/src/components/Login.jsx
+++ yoga/src/components/Login.jsx
@@ -1,28 +1,61 @@
-// src/components/Login.js
+
 import React from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-const Login = () => {
+function Login(){
 
-
-let navigate=useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // const [payload, setPayload] = useState()
+  let navigate=useNavigate();
 function handleClick(){
-  console.log("hi there")
-  navigate("/login/home");
+
+ 
+ const endpoint = 'http://127.0.0.1:8000/home/login/'
+      
+      const payload = {
+         useremail: email,
+         userpassword: password
+      }
+      console.log(payload)
+
+      fetch(endpoint,
+          {
+              method: 'POST',
+              headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(payload)
+          })
+          .then(response => response.json())
+          .then(data => {
+             if(data==="SUCCESS")
+             {
+                 
+              navigate("/login/home");
+             }
+             else{
+                 window.alert("Please check your login")
+             }
+          })
+
+  // navigate("/login/home");
 }
   return (
     <div className="form-container">
       <h2>Login</h2>
-      <form>
+      
         <label>
           Email:
-          <input type="email" />
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
         </label>
         <label>
           Password:
-          <input type="password" />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
         </label>
         <button onClick={handleClick}>Login</button>
-      </form>
+      
     </div>
   );
 };
