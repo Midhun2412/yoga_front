32214f4ebdab1c494e125b627a430169126dcbc8
online <mohanmidhun084@gmail.com>
2024-02-29 07:32:06 +0530

    ui updated

--- yoga/src/components/Landing/Feature1.jsx
+++ yoga/src/components/Landing/Feature1.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './feature1.css'
+import F1 from './feature1.png'
 const Feature1 = () => {
   return (
     <div className='feature1-flex'>
-     <div className='f' id='f1'>hi</div>
-     <div className='f' id='f2'></div>
+     <div className='f' id='f1'> <img id='img1' src={F1} alt="Tadasana" height={500}  width={500}/></div>
+     <div className='f' id='f2'>
+    <h2>FEATURE</h2>
+    <h1>Easy-touse AI technology</h1>
+    <p>From pose guidance to form correction, we offer comprehensive </p>
+     </div>
     </div>
   )
 }
--- yoga/src/index.js
+++ yoga/src/index.js
@@ -8,7 +8,7 @@ import WebcamComponent from './components/WebcamComponent';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <WebcamComponent />
+    <App />
   </React.StrictMode>
 );
 
